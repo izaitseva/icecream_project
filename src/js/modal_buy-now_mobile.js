1d422c7eec44b25e87a48b3f4b7d1ba095e50494
Dmytro Savych <sava.savych@gmail.com>
2022-08-15 19:26:55 +0300

    Переведено модалку на англійську мову + полагоджено модалку

--- src/js/modal_buy-now_mobile.js
+++ src/js/modal_buy-now_mobile.js
@@ -2,7 +2,7 @@
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open_buy-now_mobile]"),
     closeModalBtn: document.querySelector("[data-modal-close_buy-now_mobile]"),
-    modal: document.querySelector("[data-menu]"),
+    modal: document.querySelector("[data-modal_buy-now]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
